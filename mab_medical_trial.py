9cad86a64b46ce1cc506647b36716838c2a03eb7
Guadalupe Cruz <113732018+LupeCruz16@users.noreply.github.com>
2024-02-02 21:38:41 +0000

    Reverting mab medical trial

--- mab_medical_trial.py
+++ mab_medical_trial.py
@@ -1,36 +1,58 @@
 import random
 import matplotlib.pyplot as plt
 
+'''
+To view generated graph install library as command:
+
+pip install matplotlib
+
+'''
+
 Green = [4.77317728, 5.99791051, 5.76776377, 4.47913849, 6.21411927,
-         6.84915318, 8.44082357, 6.15266159, 6.97135381, 7.43452167]
+6.84915318, 8.44082357, 6.15266159, 6.97135381, 7.43452167]
 
 Blue = [6.00449072, 3.34005839, 6.71096916, 4.11113061, 5.68416528,
-        3.88539945, 3.51181469, 3.67426432, 4.98069804, 4.41366311]
+3.88539945, 3.51181469, 3.67426432, 4.98069804, 4.41366311]
 
 Red = [6.36086896, 5.65584783, 7.62912922, 13.29826146, 5.99876216,
-       8.14484021, 9.74488991, 6.616229, 14.26793535, 0.98932393]
+8.14484021, 9.74488991, 6.616229, 14.26793535, 0.98932393]
 
-# Initial Q values for each action
+# Initial values 
 QG = [0]
 QB = [0]
 QR = [0]
 
-# Number of times an action has been selected
-N_counts = [0, 0, 0]
+# Number of times a choice has been selected
+N_counts = [0, 0, 0]  # NG, NB, NR
 
-# Epsilon for exploration
-EPSILON = 0.1  # Slightly increased for more exploration
+#Epsilon 
+EPSILON = 0.01
 
-# Learning rate (alpha)
-ALPHA = 0.1
+'''
+    Steps
 
+    1. Find the greedy action => find the action to take
+    2. Generate the random reward
+    --> from the list
+    -- follow a distribution
+    3. Update the Q from the action
+
+    All three steps are functions 
+'''
+
+# Choose action function
 def choose_action(QG, QB, QR, EPSILON):
+    Q_values = [QG[-1], QB[-1], QR[-1]]
     if random.random() < EPSILON:
+        # Exploration: choose a random action
         return random.randint(0, 2)
     else:
-        Q_values = [QG[-1], QB[-1], QR[-1]]
-        return Q_values.index(max(Q_values))
+        # Exploitation: choose the greedy action (max Q value)
+        max_value = max(Q_values)
+        greedy_actions = [i for i, q in enumerate(Q_values) if q == max_value]
+        return random.choice(greedy_actions)
 
+# Reward function
 def get_reward(action):
     if action == 0:
         return random.choice(Green)
@@ -39,33 +61,36 @@ def get_reward(action):
     else:
         return random.choice(Red)
 
-def update_Q(Q, reward, alpha):
-    return Q + alpha * (reward - Q)
-
-for _ in range(100):
+# Main loop
+for _ in range(100):  # Loop 100 times
     action = choose_action(QG, QB, QR, EPSILON)
     reward = get_reward(action)
-
+    
+    # Update Q values based on the average
     if action == 0:
-        QG.append(update_Q(QG[-1], reward, ALPHA))
+        QG.append((QG[-1] * N_counts[0] + reward) / (N_counts[0] + 1))
+        N_counts[0] += 1
     elif action == 1:
-        QB.append(update_Q(QB[-1], reward, ALPHA))
+        QB.append((QB[-1] * N_counts[1] + reward) / (N_counts[1] + 1))
+        N_counts[1] += 1
     else:
-        QR.append(update_Q(QR[-1], reward, ALPHA))
+        QR.append((QR[-1] * N_counts[2] + reward) / (N_counts[2] + 1))
+        N_counts[2] += 1
 
-    N_counts[action] += 1
-
+# Print the final counts
 print(f"Final Counts: Green selected {N_counts[0]} times, Blue selected {N_counts[1]} times, Red selected {N_counts[2]} times")
 
+# Generate the plot
 plt.figure(figsize=(12, 6))
-plt.plot(range(len(QG)), QG, label='Green', color='green')
-plt.plot(range(len(QB)), QB, label='Blue', color='blue')
-plt.plot(range(len(QR)), QR, label='Red', color='red')
+plt.plot(range(len(QG)), QG, label='Green')
+plt.plot(range(len(QB)), QB, label='Blue')
+plt.plot(range(len(QR)), QR, label='Red')
 
 plt.xlabel('Number of times the choice has been taken')
-plt.ylabel('Q Value')
-plt.title('Q Value vs. Number of Actions Taken')
+plt.ylabel('Average Q Value')
+plt.title('Average Q Value vs. Number of Actions Taken')
 plt.legend()
 plt.xlim([0, 100])
 
+plt.savefig('reward_plot.png')
 plt.show()
